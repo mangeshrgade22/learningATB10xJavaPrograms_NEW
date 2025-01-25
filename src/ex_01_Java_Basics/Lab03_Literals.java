395bf3815669a56169bd7d3037a5ecd7967e1592
MANGESH <mangeshrgade22@gmaii.com>
2025-01-25 12:59:42 +0530

    todays task

---
+++ src/ex_01_Java_Basics/Lab03_Literals.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab03_Literals {
+    public static void main(String[] args) {
+       // int age=35;//35--literals
+        //age=65;//65-literals can change the value
+        final int age=18;//fianl-constant value does,t change.
+        age=19;//19--not possible due to final age is constant .
+
+        System.out.println(age);
+    }
+
+}
---
+++ src/lab02_Arithmatic_operators.java
@@ -0,0 +1,15 @@
+public class lab02_Arithmatic_operators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        float c = 2f;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b); // 6
+        System.out.println(a/c); // 6.6666665
+
+
+    }
+}
